e9c5bc53746d80b42c15fb742cd8c0b48f4412e5
Jingang Miao <9098862+miaojingang@users.noreply.github.com>
2020-09-14 10:11:11 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# Copyright 2020 The mc Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='mc',
+    version='0.1',
+    description='Package for misclassification correction',
+    author='Jingang Miao',
+    url='https://github.com/facebookresearch/mc',
+    license='Apache 2.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy >= 1.11.1',
+        'pandas',
+        'scipy',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
