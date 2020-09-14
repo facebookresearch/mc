69d7b54a5b88db8169039c506789c0c766bdee6c
Jingang Miao <9098862+miaojingang@users.noreply.github.com>
2020-09-14 10:44:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The mc Authors.
+# Copyright (c) Facebook, Inc. and its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-
 
 from setuptools import find_packages
 from setuptools import setup
